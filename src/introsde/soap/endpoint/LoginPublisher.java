4f7a0b1e5f86e49bd3c3dab250851d190eaefdca
Remco Kortekaas <rackortekaas@gmail.com>
2017-02-13 17:05:20 +0100

    publisher edited

--- src/introsde/soap/endpoint/LoginPublisher.java
+++ src/introsde/soap/endpoint/LoginPublisher.java
@@ -1,9 +1,5 @@
 package introsde.soap.endpoint;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.URISyntaxException;
-
 import javax.xml.ws.Endpoint;
 
 import introsde.soap.ws.LoginImpl;
@@ -11,24 +7,8 @@ import introsde.soap.ws.LoginImpl;
 //Endpoint publisher
 public class LoginPublisher{
 
-    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
-        String PROTOCOL = "http://";
-        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
-        if (HOSTNAME.equals("127.0.0.1"))
-        {
-            HOSTNAME = "localhost";
-        }
-        String PORT = "6900";
-        String BASE_URL = "/ws/login";
-
-        if (String.valueOf(System.getenv("PORT")) != "null"){
-            PORT=String.valueOf(System.getenv("PORT"));
-        }
-
-        String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
-        System.out.println("Starting Inlog Service...");
-        System.out.println("--> Published at = "+endpointUrl);
-        Endpoint.publish(endpointUrl, new LoginImpl());
-        }
+    public static void main(String[] args) {
+	   Endpoint.publish("https://sleepy-fortress-27067.herokuapp.com/project/ws/login", new LoginImpl());
+    }
 
 }
