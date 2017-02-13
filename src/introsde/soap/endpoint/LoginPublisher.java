a74f3d3437db0029fff9625102f4ffbdabb37eb7
Remco Kortekaas <rackortekaas@gmail.com>
2017-02-13 17:54:59 +0100

    loginpublisher updated

--- src/introsde/soap/endpoint/LoginPublisher.java
+++ src/introsde/soap/endpoint/LoginPublisher.java
@@ -1,14 +1,33 @@
 package introsde.soap.endpoint;
 
-import javax.xml.ws.Endpoint;
-
 import introsde.soap.ws.LoginImpl;
 
-//Endpoint publisher
-public class LoginPublisher{
+import java.net.InetAddress;
+import java.net.URISyntaxException;
+import java.io.IOException;
 
-    public static void main(String[] args) {
-	   Endpoint.publish("http://localhost:6900/ws/login", new LoginImpl());
-    }
+import javax.xml.ws.Endpoint;
+
+public class LoginPublisher {
+    
+    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
+        String PROTOCOL = "http://";
+        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
+        if (HOSTNAME.equals("127.0.0.1"))
+        {
+            HOSTNAME = "localhost";
+        }
+        String PORT = "6902";
+        String BASE_URL = "/ws/login";
+
+        if (String.valueOf(System.getenv("PORT")) != "null"){
+            PORT=String.valueOf(System.getenv("PORT"));
+        }
+
+        String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
+        System.out.println("Starting People Service...");
+        System.out.println("--> Published at = "+endpointUrl);
+        Endpoint.publish(endpointUrl, new LoginImpl());
+        }
 
 }
