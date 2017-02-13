b4599231a9f29c25634f219f873cbd4bcb56eaa3
Remco Kortekaas <rackortekaas@gmail.com>
2017-02-13 17:44:35 +0100

    changes back

--- src/introsde/soap/endpoint/LoginPublisher.java
+++ src/introsde/soap/endpoint/LoginPublisher.java
@@ -6,18 +6,9 @@ import introsde.soap.ws.LoginImpl;
 
 //Endpoint publisher
 public class LoginPublisher{
-	
-        public static String endpointUrl = "https://sleepy-fortress-27067.herokuapp.com/ws/login";
-//        public static String PORT = "";
-//        public static String BASE_URL = "/ws/login";
 
-//        public static String getEndpointURL() {
-//            return SERVER_URL+BASE_URL;
-//        }
+    public static void main(String[] args) {
+	   Endpoint.publish("http://localhost:6900/ws/login", new LoginImpl());
+    }
 
-        public static void main(String[] args) {
-            System.out.println("Starting Login Service...");
-            System.out.println("--> Published at = "+endpointUrl);
-            Endpoint.publish(endpointUrl, new LoginImpl());
-        }
 }
