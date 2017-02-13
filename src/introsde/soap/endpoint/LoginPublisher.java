a39f7ddf0047461e108bde92c6c12ce0121df530
Remco Kortekaas <rackortekaas@gmail.com>
2017-02-13 17:20:52 +0100

    build.xml updated for soap webservice

--- src/introsde/soap/endpoint/LoginPublisher.java
+++ src/introsde/soap/endpoint/LoginPublisher.java
@@ -6,9 +6,19 @@ import introsde.soap.ws.LoginImpl;
 
 //Endpoint publisher
 public class LoginPublisher{
+	
+        public static String SERVER_URL = "http://localhost";
+        public static String PORT = "6900";
+        public static String BASE_URL = "/ws/login";
 
-    public static void main(String[] args) {
-	   Endpoint.publish("https://sleepy-fortress-27067.herokuapp.com/project/ws/login", new LoginImpl());
-    }
+        public static String getEndpointURL() {
+            return SERVER_URL+":"+PORT+BASE_URL;
+        }
 
+        public static void main(String[] args) {
+            String endpointUrl = getEndpointURL();
+            System.out.println("Starting People Service...");
+            System.out.println("--> Published at = "+endpointUrl);
+            Endpoint.publish(endpointUrl, new LoginImpl());
+        }
 }
